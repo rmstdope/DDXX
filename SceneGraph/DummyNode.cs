bdb1e612e1a2591c17b192341c31b8325e54f0ee
Henrik Kurelid <henrik@kurelid.se>
2006-08-06 20:47:04 +0000

    Missed one file in last commit.

---
+++ SceneGraph/DummyNode.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SceneGraph
+{
+    public class DummyNode : NodeBase
+    {
+        public DummyNode(string name)
+            : base(name)
+        {
+        }
+
+        protected override void StepNode()
+        {
+        }
+
+        protected override void RenderNode()
+        {
+        }
+    }
+}
