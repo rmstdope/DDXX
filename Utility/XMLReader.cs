ae488bc9575ce6efb39f2dd888fb3e76a3ce88da
Levon Saldamli <unknown@unknown.com>
2006-08-15 13:10:25 +0000

    Initial XML reader class added.

---
+++ Utility/XMLReader.cs
@@ -0,0 +1,158 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace Utility
+{
+    public enum ParameterType
+    {
+        Unknown = 0, Integer, Float, String
+    }
+
+    public struct Parameter
+    {
+        public string Name;
+        public string Value;
+        public ParameterType Type;
+        public Parameter(string name, ParameterType type, string value)
+        {
+            this.Name = name;
+            this.Type = type;
+            this.Value = value;
+        }
+    }
+
+    public class XMLReader
+    {
+        private Stream inputStream;
+        private XmlReader reader;
+        private string effectName;
+
+        public string EffectName
+        {
+            get { return effectName; }
+        }
+
+        public void Start(Stream input)
+        {
+            this.inputStream = input;
+            XmlReaderSettings settings = new XmlReaderSettings();
+            settings.ConformanceLevel = ConformanceLevel.Document;
+            settings.IgnoreWhitespace = true;
+            settings.IgnoreComments = true;
+            reader = XmlReader.Create(inputStream, settings);
+            //reader.MoveToContent();
+            while (reader.Read())
+            {
+                // PrintElement(reader);
+                if (reader.NodeType != XmlNodeType.Element)
+                    continue;
+                if (reader.Name == "Effects")
+                {
+                    return;
+                }
+                else
+                {
+                    throw new XmlException("Expected <Effects>, found <" + reader.Name + ">");
+                }
+            }
+        }
+
+        private void PrintElement()
+        {
+            Console.WriteLine("{0}: {1} = {2}",
+                reader.NodeType.ToString(), reader.Name, reader.Value);
+            for (int i = 0, c = reader.AttributeCount; i < c; i++)
+            {
+                reader.MoveToAttribute(i);
+                Console.WriteLine("  {0} = {1} ", reader.Name, reader.Value);
+            }
+            reader.MoveToElement();
+        }
+
+        public void Close()
+        {
+            reader.Close();
+            inputStream.Close();
+        }
+
+        public bool NextEffect()
+        {
+            while (reader.Read())
+            {
+                if (reader.NodeType == XmlNodeType.Element && reader.Name == "Effect")
+                {
+                    effectName = reader.GetAttribute("name");
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public Dictionary<string, Parameter> GetParameters()
+        {
+            Dictionary<string, Parameter> parameters = new Dictionary<string, Parameter>();
+            while (reader.Read())
+            {
+                if (reader.NodeType == XmlNodeType.EndElement && reader.Name == "Effect")
+                {
+                    break;
+                }
+                if (reader.NodeType == XmlNodeType.Element && reader.Name == "Parameter")
+                {
+                    Parameter p = ReadParameter();
+                    parameters.Add(p.Name, p);
+                }
+            }
+            return parameters;
+        }
+
+        private Parameter ReadParameter()
+        {
+            bool keepOn = reader.MoveToFirstAttribute();
+            ParameterType parameterType = ParameterType.Unknown;
+            string parameterName = null;
+            string parameterValue = "";
+            while (keepOn)
+            {
+                string name = reader.Name;
+                string value = reader.Value;
+                switch (name)
+                {
+                    case "name": 
+                        parameterName = value; 
+                        break;
+                    default: 
+                        parameterType = GetParameterType(name);
+                        if (parameterType != ParameterType.Unknown)
+                        {
+                            parameterValue = value;
+                        }
+                        break;
+                }
+                keepOn = reader.MoveToNextAttribute();
+            }
+            if (parameterName != null && parameterType != ParameterType.Unknown)
+            {
+                return new Parameter(parameterName, parameterType, parameterValue);
+            }
+            else
+            {
+                throw new XmlException("Failed to parse parameter");
+            }
+        }
+
+        private ParameterType GetParameterType(string name)
+        {
+            switch (name)
+            {
+                case "int": return ParameterType.Integer;
+                case "float": return ParameterType.Float;
+                case "string": return ParameterType.String;
+                default: return ParameterType.Unknown;
+            }
+        }
+    }
+}
---
+++ Utility/XMLReaderTest.cs
@@ -0,0 +1,156 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+using NUnit.Framework;
+using NMock2;
+using System.Xml;
+
+// TODO:
+
+namespace Utility
+{
+    [TestFixture]
+    public class XMLReaderTest
+    {
+        List<string> tempFiles = new List<string>();
+        XMLReader reader;
+        static string twoEffectContents =
+@"<Effects>
+<Effect name=""fooeffect"">
+<Parameter name=""fooparam"" int=""3"" />
+<Parameter name=""barparam"" float=""4.3f"" />
+</Effect>
+<Effect name=""bareffect"">
+<Parameter name=""goo"" string=""string value"" />
+</Effect>
+</Effects>
+";
+
+        [TearDown]
+        public void TearDown()
+        {
+            foreach (string s in tempFiles)
+            {
+                Console.WriteLine("deleting temp file: " + s);
+                File.Delete(s);
+            }
+        }
+
+        [Test]
+        public void TestNoEffectsNode()
+        {
+            try
+            {
+                reader = OpenXML(@"<Foo></Foo>");
+                Assert.Fail("Expected XmlException because no <Effects> node found");
+                reader.Close();
+            }
+            catch (XmlException)
+            {
+            }
+        }
+
+        [Test]
+        public void TestNextEffect()
+        {
+            reader = OpenXML(twoEffectContents);
+            Assert.IsTrue(reader.NextEffect());
+            Assert.AreEqual("fooeffect", reader.EffectName);
+            Assert.IsTrue(reader.NextEffect());
+            Assert.AreEqual("bareffect", reader.EffectName);
+            Assert.IsFalse(reader.NextEffect());
+            reader.Close();
+        }
+
+        [Test]
+        public void TestGetParameters()
+        {
+            reader = OpenXML(twoEffectContents);
+            reader.NextEffect();
+            Assert.AreEqual("fooeffect", reader.EffectName);
+            Dictionary<string, Parameter> parameters = reader.GetParameters();
+            Assert.AreEqual(2, parameters.Count);
+            Parameter parameter;
+            Assert.IsTrue(parameters.TryGetValue("fooparam", out parameter));
+            Assert.AreEqual(ParameterType.Integer, parameter.Type);
+            Assert.AreEqual("3", parameter.Value);
+            Assert.IsTrue(parameters.TryGetValue("barparam", out parameter));
+            Assert.AreEqual(ParameterType.Float, parameter.Type);
+            Assert.AreEqual("4.3f", parameter.Value);
+            reader.Close();
+        }
+
+        [Test]
+        public void TestWrongParams()
+        {
+            string wrongParamContents =
+@"<Effects><Effect><Parameter name=""foo"" /></Effect></Effects>";
+            reader = OpenXML(wrongParamContents);
+            reader.NextEffect();
+            try
+            {
+                reader.GetParameters();
+                Assert.Fail("Expected exception due to missing parameter type");
+            }
+            catch (XmlException)
+            {
+            }
+            finally
+            {
+                reader.Close();
+            }
+        }
+
+        [Test]
+        public void TestWrongParamType()
+        {
+            string wrongParamTypeContents =
+@"<Effects><Effect><Parameter name=""foo"" sunktype=""44"" /></Effect></Effects>";
+            reader = OpenXML(wrongParamTypeContents);
+            reader.NextEffect();
+            try
+            {
+                reader.GetParameters();
+                Assert.Fail("Expected exception due to unknown parameter type");
+            }
+            catch (XmlException)
+            {
+            }
+            finally
+            {
+                reader.Close();
+            }
+        }
+
+
+        private XMLReader OpenXML(string contents)
+        {
+            string tempFilename = WriteTempFile(contents);
+            Stream inputStream = new FileStream(tempFilename, FileMode.Open);
+            XMLReader reader = new XMLReader();
+            try
+            {
+                reader.Start(inputStream);
+                return reader;
+            }
+            catch (Exception e)
+            {
+                reader.Close();
+                throw e;
+            }
+        }
+
+        internal string WriteTempFile(string contents)
+        {
+            string tempFilename = Path.GetTempFileName();
+            StreamWriter writer = new StreamWriter(tempFilename);
+            writer.Write(contents);
+            writer.Close();
+            tempFiles.Add(tempFilename);
+            return tempFilename;
+        }
+
+    }
+}
