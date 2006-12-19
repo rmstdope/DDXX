7218fea525498bdd7de436b4ed0b372e093d8f99
Henrik Kurelid <henrik@kurelid.se>
2006-12-19 20:03:08 +0000

    Added skinned mesh for testing #36.

--- Effects/Test.fx
+++ Effects/Test.fx
@@ -12,12 +12,13 @@ struct InputPS
 {
 	float4 Position				:	POSITION;			// Vertex position 
 	float2 TextureCoord		:	TEXCOORD0;		// Vertex texture coords 
-	float3 LightVector		:	TEXCOORD1;		// Light vector in tangent space
+};
 
-	//float3 LightVector	:	TEXCOORD1;		// Light vector in tangent space
-	//float3 EyeVector		:	TEXCOORD2;		// Eye vector in tangent space
-	//float2 AttCoord1		:	TEXCOORD3;		// X and Y coordinates for attenuaton texture
-	//float2 AttCoord2		:	TEXCOORD4;		// Z coordinate for attenuation texture
+struct NormalMappingInputPS
+{
+	float4 Position				:	POSITION;			// Vertex position 
+	float2 TextureCoord		:	TEXCOORD0;		// Vertex texture coords 
+	float3 LightVector		:	TEXCOORD1;		// Light vector in tangent space
 };
 
 
@@ -25,6 +26,19 @@ InputPS
 VertexShader(InputVS input)
 {
 	InputPS output;
+
+	// Transform the position from object space to homogeneous projection space
+	output.Position = mul(input.Position, WorldViewProjectionT);
+
+	output.TextureCoord = input.TextureCoord;
+	
+	return output;    
+}
+
+NormalMappingInputPS
+NormalMappingVertexShader(InputVS input)
+{
+	NormalMappingInputPS output;
 
 	// Transform the position from object space to homogeneous projection space
 	output.Position = mul(input.Position, WorldViewProjectionT);
@@ -37,24 +51,21 @@ VertexShader(InputVS input)
 
 	output.TextureCoord = input.TextureCoord;
 	
-	//float3 N = normalize(mul(input.Normal, (float3x3)WorldT));
-
-	//float3 L1 = normalize(LightPosition[0] - mul(input.Position, WorldT));
-	//float3 L2 = normalize(LightPosition[1] - mul(input.Position, WorldT));
-	//output.LightVector = max(0, dot(N, L1)) * LightDiffuseColor[0] + max(0, dot(N, L2)) * LightDiffuseColor[1];
-	//float3 V = normalize(EyePosition.xyz - input.Position.xyz);
-	//float3 H = normalize(V + normalize(output.L));
-	
 	return output;    
 }
 
 float4
 PixelShader(InputPS input) : COLOR0
 {
+	return AmbientColor * tex2D(BaseTextureSampler, input.TextureCoord.xy);
+}
+
+float4
+NormalMappingPixelShader(NormalMappingInputPS input) : COLOR0
+{
 	float3 normal = tex2D(NormalTextureSampler, input.TextureCoord.xy).rgb * 2.0 - 1.0;
 	float diffuse = dot(normal, input.LightVector);
-	//return max(0, diffuse);
-	return /*AmbientColor*/ diffuse * tex2D(BaseTextureSampler, input.TextureCoord.xy);
+	return diffuse * tex2D(BaseTextureSampler, input.TextureCoord.xy);
 }
 
 technique Test
@@ -94,3 +105,32 @@ technique TransparentText
 	}
 }
 
+technique NoSkinning
+{
+	pass BasePass
+	{
+		VertexShader			= compile vs_2_0 VertexShader();
+		PixelShader				= compile ps_2_0 PixelShader();
+		AlphaBlendEnable	= false;
+		CullMode					= CCW;
+		FillMode					= Solid;//<FillMode>;
+		ZEnable						=	true;
+		ZFunc							= Less;
+		StencilEnable			= false; //true;
+	}
+}
+
+technique NormalMapping
+{
+	pass BasePass
+	{
+		VertexShader			= compile vs_2_0 NormalMappingVertexShader();
+		PixelShader				= compile ps_2_0 NormalMappingPixelShader();
+		AlphaBlendEnable	= false;
+		CullMode					= CCW;
+		FillMode					= Solid;//<FillMode>;
+		ZEnable						=	true;
+		ZFunc							= Less;
+		StencilEnable			= false; //true;
+	}
+}
--- EngineTest/TestEffect.cs
+++ EngineTest/TestEffect.cs
@@ -17,32 +17,11 @@ namespace EngineTest
     {
         private FloaterSystem ps;
         private CameraNode camera;
-        private ISprite sprite;
-        private ITexture texture;
-        private Color colorValue = Color.FromArgb(100, 20, 30, 40);
-        private int intValue = 1;
-        private Vector3 vector3Value = new Vector3(1, 1, 1);
         private Scene scene;
-        private IFont font;
         private ModelNode mesh;
 
-        public Vector3 Vector3Value
-        {
-            get { return vector3Value; }
-            set { vector3Value = value; }
-        }
-
-        public int IntValue
-        {
-            get { return intValue; }
-            set { intValue = value; }
-        }
-
-        public Color ColorValue
-        {
-            get { return colorValue; }
-            set { colorValue = value; }
-        }
+        private ModelNode modelSkinning;
+        private ModelNode modelNoSkinning;
 
         public TestEffect(float startTime, float endTime) 
             : base(startTime, endTime)
@@ -54,26 +33,17 @@ namespace EngineTest
         {
             base.Initialize();
 
-            sprite = D3DDriver.Factory.CreateSprite(Device);
-            texture = D3DDriver.TextureFactory.CreateFromFile("BlurBackground.jpg");
             scene.AmbientColor = new ColorValue(1.0f, 1.0f, 1.0f);
 
             camera = new CameraNode("Camera");
             camera.WorldState.MoveForward(-400.0f);
-            camera.WorldState.MoveRight(50.0f);
+            //camera.WorldState.MoveRight(50.0f);
             scene.AddNode(camera);
             scene.ActiveCamera = camera;
 
             ps = new FloaterSystem("System");
             ps.Initialize(50, 200.0f, null);//"BlurBackground.jpg");
             scene.AddNode(ps);
-
-            // Render font
-            FontDescription description = new FontDescription();
-            description.FaceName = "Arial";
-            description.Height = 12;
-            font = D3DDriver.Factory.CreateFont(Device, description);
-            font.DrawText(null, "Hejsan", new Point(100, 100), Color.FloralWhite);
 
             // Create mesh
             IEffect effect = D3DDriver.EffectFactory.CreateFromFile("Test.fxo");
@@ -82,6 +52,24 @@ namespace EngineTest
             mesh = new ModelNode("Text1", model, effectHandler);
             scene.AddNode(mesh);
             //mesh.WorldState.Tilt(-(float)Math.PI / 2.0f);
+
+            modelNoSkinning = new ModelNode("No Skinning",
+                ModelFactory.FromFile("tiny.x", ModelFactory.Options.None),
+                new EffectHandler(EffectFactory.CreateFromFile("Test.fxo"), "NoSkinning"));
+            modelNoSkinning.WorldState.Scale(0.3f);
+            modelNoSkinning.WorldState.MoveRight(-50);
+            modelNoSkinning.WorldState.Roll((float)Math.PI);
+            modelNoSkinning.WorldState.Tilt((float)Math.PI / 2);
+            scene.AddNode(modelNoSkinning);
+
+            modelSkinning = new ModelNode("No Skinning",
+                ModelFactory.FromFile("tiny.x", ModelFactory.Options.None),
+                new EffectHandler(EffectFactory.CreateFromFile("Test.fxo"), "NoSkinning"));
+            modelSkinning.WorldState.Scale(0.3f);
+            modelSkinning.WorldState.MoveRight(50);
+            modelSkinning.WorldState.Roll((float)Math.PI);
+            modelSkinning.WorldState.Tilt((float)Math.PI / 2);
+            scene.AddNode(modelSkinning);
         }
 
         public override void StartTimeUpdated()
@@ -109,15 +97,6 @@ namespace EngineTest
         {
             IDevice device = D3DDriver.GetInstance().Device;
             scene.Render();
-
-            //int sWidth = device.PresentationParameters.BackBufferWidth;
-            //int sHeight = device.PresentationParameters.BackBufferHeight;
-            //int tWidth = texture.GetSurfaceLevel(0).Description.Width;
-            //int tHeight = texture.GetSurfaceLevel(0).Description.Height;
-            //sprite.Begin(SpriteFlags.AlphaBlend);
-            //sprite.Draw2D(texture, Rectangle.Empty, new SizeF(sWidth * 1.9f, sHeight * 1.9f), new PointF(tWidth / 2, tHeight / 2), Time.StepTime * 0.22f, new PointF(sWidth / 2, sHeight / 2), Color.FromArgb(30, 255, 150, 150));
-            //sprite.Draw2D(texture, Rectangle.Empty, new SizeF(sWidth * 1.9f, sHeight * 1.9f), new PointF(tWidth / 2, tHeight / 2), -Time.StepTime * 0.15f, new PointF(sWidth / 2, sHeight / 2), Color.FromArgb(30, 150, 255, 150));
-            //sprite.End();
         }
 
     }
