e274432f06336d592b9bb06b8eaf7bc2689e0488
Henrik Kurelid <henrik@kurelid.se>
2007-05-03 12:03:43 +0000

    Finished implementation of TextFadingEffect.

--- DemoEffects/TextFadingEffect.cs
+++ DemoEffects/TextFadingEffect.cs
@@ -4,6 +4,9 @@ using System.Text;
 using Dope.DDXX.DemoFramework;
 using Dope.DDXX.Graphics;
 using Microsoft.DirectX.Direct3D;
+using System.Drawing;
+using Microsoft.DirectX;
+using Dope.DDXX.Utility;
 
 namespace Dope.DDXX.DemoEffects
 {
@@ -11,20 +14,96 @@ namespace Dope.DDXX.DemoEffects
     {
         private ISprite sprite;
         private IFont font;
+        private string fontName;
+        private int fontHeight;
+        private Vector2 textPosition;
+        private string text;
+        private Color textColor;
+        private float fadeInLength;
+        private float fadeOutLength;
+
+        public float FadeOutLength
+        {
+            get { return fadeOutLength; }
+            set { fadeOutLength = value; }
+        }
+
+        public float FadeInLength
+        {
+            get { return fadeInLength; }
+            set { fadeInLength = value; }
+        }
+
+        public Color TextColor
+        {
+            get { return textColor; }
+            set { textColor = value; }
+        }
+
+        public string Text
+        {
+            get { return text; }
+            set { text = value; }
+        }
+
+        public Vector2 TextPosition
+        {
+            get { return textPosition; }
+            set { textPosition = value; }
+        }
+
+        public int FontHeight
+        {
+            get { return fontHeight; }
+            set 
+            {
+                if (font != null && fontHeight != value)
+                {
+                    fontHeight = value;
+                    CreateFont();
+                }
+                fontHeight = value;
+            }
+        }
+
+        public string FontName
+        {
+            get { return fontName; }
+            set
+            {
+                if (font != null && fontName != value)
+                {
+                    fontName = value;
+                    CreateFont();
+                }
+                fontName = value;
+            }
+        }
 
         public TextFadingEffect(float startTime, float endTime)
             : base(startTime, endTime)
         {
+            TextPosition = new Vector2(0.5f, 0.5f);
+            Text = "Default";
+            FontHeight = 50;
+            FontName = "Arial";
+            textColor = Color.White;
         }
 
         protected override void Initialize()
         {
+            CreateFont();
+            sprite = GraphicsFactory.CreateSprite(Device);
+        }
+
+        private void CreateFont()
+        {
             FontDescription description = new FontDescription();
-            description.FaceName = "Arial";
-            description.Height = 25;
+            description.FaceName = fontName;
+            description.Height = fontHeight;
+            description.Quality = FontQuality.AntiAliased;
 
             font = GraphicsFactory.CreateFont(Device, description);
-            sprite = GraphicsFactory.CreateSprite(Device);
         }
 
         public override void Step()
@@ -33,6 +112,32 @@ namespace Dope.DDXX.DemoEffects
 
         public override void Render()
         {
+            int alpha = GetFadeAlpha();
+            Rectangle textRectangle = GetTextRectangle();
+            sprite.Begin(SpriteFlags.AlphaBlend);
+            font.DrawText(sprite, text, textRectangle, 
+                DrawTextFormat.Center | DrawTextFormat.VerticalCenter, Color.FromArgb(alpha, textColor));
+            sprite.End();
+        }
+
+        private Rectangle GetTextRectangle()
+        {
+            int x = (int)(textPosition.X * Device.Viewport.Width - 500);
+            int y = (int)(textPosition.Y * Device.Viewport.Height - 500);
+            Rectangle textRectangle = new Rectangle(x, y, 1000, 1000);
+            return textRectangle;
+        }
+
+        private int GetFadeAlpha()
+        {
+            int alpha = 255;
+            float time = Time.CurrentTime - StartTime;
+            if (time < FadeInLength)
+                alpha = (int)(255 * (time / FadeInLength));
+            time = Time.CurrentTime - (EndTime - FadeOutLength);
+            if (time > 0)
+                alpha = 254 - (int)(255 * (time / FadeOutLength));
+            return alpha;
         }
     }
 }
--- DemoEffects/TextFadingEffectTest.cs
+++ DemoEffects/TextFadingEffectTest.cs
@@ -5,12 +5,19 @@ using NUnit.Framework;
 using Dope.DDXX.Graphics;
 using NMock2;
 using Microsoft.DirectX.Direct3D;
+using System.IO;
+using System.Drawing;
+using Microsoft.DirectX;
+using Dope.DDXX.Utility;
 
 namespace Dope.DDXX.DemoEffects
 {
     [TestFixture]
     public class TextFadingEffectTest
     {
+        private const int VIEWPORT_WIDTH = 444;
+        private const int VIEWPORT_HEIGHT = 666;
+
         private TextFadingEffect textFadingEffect;
 
         private Mockery mockery;
@@ -18,6 +25,8 @@ namespace Dope.DDXX.DemoEffects
         private IDevice device;
         private ISprite sprite;
         private IFont font;
+
+        private Viewport viewport;
 
         [SetUp]
         public void SetUp()
@@ -27,7 +36,13 @@ namespace Dope.DDXX.DemoEffects
             device = mockery.NewMock<IDevice>();
             sprite = mockery.NewMock<ISprite>();
             font = mockery.NewMock<IFont>();
+            viewport = new Viewport();
+            viewport.Width = VIEWPORT_WIDTH;
+            viewport.Height = VIEWPORT_HEIGHT;
+            Stub.On(device).GetProperty("Viewport").Will(Return.Value(viewport));
 
+            Time.Initialize();
+            Time.Pause();
             textFadingEffect = new TextFadingEffect(0, 10);
         }
 
@@ -38,25 +53,257 @@ namespace Dope.DDXX.DemoEffects
         }
 
         [Test]
-        public void TestInitialize()
+        public void TestInitializeArial25()
         {
-            FontDescription description = new FontDescription();
-            description.FaceName = "Arial";
-            description.Height = 25;
-            Expect.Once.On(graphicsFactory).
-                Method("CreateSprite").Will(Return.Value(sprite));
-            Expect.Once.On(graphicsFactory).
-                Method("CreateFont").With(device, description).Will(Return.Value(font));
-            textFadingEffect.Initialize(graphicsFactory, device);
+            InitializeEffect("Arial", 25);
         }
 
         [Test]
-        public void TestRender()
+        public void TestInitializeCourier200()
         {
-            TestInitialize();
+            InitializeEffect("Courier", 200);
+        }
 
+        [Test]
+        public void TestReInitializeSameData()
+        {
+            TestInitializeArial25();
+
+            textFadingEffect.FontName = "Arial";
+            textFadingEffect.FontHeight = 25;
+        }
+
+        [Test]
+        public void TestReInitializeDifferentName()
+        {
+            TestInitializeArial25();
+
+            ExpectFont("Courier", 25);
+            textFadingEffect.FontName = "Courier";
+        }
+
+        [Test]
+        public void TestReInitializeDifferentHeight()
+        {
+            TestInitializeArial25();
+
+            ExpectFont("Arial", 125);
+            textFadingEffect.FontHeight = 125;
+        }
+
+        [Test]
+        public void TestRenderCenter()
+        {
+            TestInitializeArial25();
+
+            textFadingEffect.TextPosition = new Vector2(0.5f, 0.5f);
+            Expect.Once.On(sprite).Method("Begin").With(SpriteFlags.AlphaBlend);
+            Expect.Once.On(font).Method("DrawText").
+                With(Is.EqualTo(sprite), Is.Anything, Is.EqualTo(new Rectangle(VIEWPORT_WIDTH / 2 - 500, VIEWPORT_HEIGHT / 2 - 500, 1000, 1000)),
+                Is.EqualTo(DrawTextFormat.Center | DrawTextFormat.VerticalCenter), Is.Anything).
+                Will(Return.Value(0));
+            Expect.Once.On(sprite).Method("End");
             textFadingEffect.Render();
         }
 
-    }
+        [Test]
+        public void TestRenderTopLeft()
+        {
+            TestInitializeArial25();
+
+            textFadingEffect.TextPosition = new Vector2(0.0f, 0.0f);
+            Expect.Once.On(sprite).Method("Begin").With(SpriteFlags.AlphaBlend);
+            Expect.Once.On(font).Method("DrawText").
+                With(Is.EqualTo(sprite), Is.Anything, Is.EqualTo(new Rectangle(-500, -500, 1000, 1000)),
+                Is.EqualTo(DrawTextFormat.Center | DrawTextFormat.VerticalCenter), Is.Anything).
+                Will(Return.Value(0));
+            Expect.Once.On(sprite).Method("End");
+            textFadingEffect.Render();
+        }
+
+        [Test]
+        public void TestRenderLowerRight()
+        {
+            TestInitializeArial25();
+
+            textFadingEffect.TextPosition = new Vector2(1.0f, 1.0f);
+            Expect.Once.On(sprite).Method("Begin").With(SpriteFlags.AlphaBlend);
+            Expect.Once.On(font).Method("DrawText").
+                With(Is.EqualTo(sprite), Is.Anything, Is.EqualTo(new Rectangle(VIEWPORT_WIDTH - 500, VIEWPORT_HEIGHT - 500, 1000, 1000)), 
+                Is.EqualTo(DrawTextFormat.Center | DrawTextFormat.VerticalCenter), Is.Anything).
+                Will(Return.Value(0));
+            Expect.Once.On(sprite).Method("End");
+            textFadingEffect.Render();
+        }
+
+        [Test]
+        public void TestText1()
+        {
+            TestInitializeArial25();
+
+            textFadingEffect.Text = "Text1";
+            Expect.Once.On(sprite).Method("Begin").With(SpriteFlags.AlphaBlend);
+            Expect.Once.On(font).Method("DrawText").
+                With(Is.EqualTo(sprite), Is.EqualTo("Text1"), Is.Anything, Is.Anything, 
+                Is.Anything).Will(Return.Value(0));
+            Expect.Once.On(sprite).Method("End");
+            textFadingEffect.Render();
+        }
+
+        [Test]
+        public void TestText2()
+        {
+            TestInitializeArial25();
+
+            textFadingEffect.Text = "Text2";
+            Expect.Once.On(sprite).Method("Begin").With(SpriteFlags.AlphaBlend);
+            Expect.Once.On(font).Method("DrawText").
+                With(Is.EqualTo(sprite), Is.EqualTo("Text2"), Is.Anything, Is.Anything,
+                Is.Anything).Will(Return.Value(0));
+            Expect.Once.On(sprite).Method("End");
+            textFadingEffect.Render();
+        }
+
+        [Test]
+        public void TestFadeInStart()
+        {
+            TestInitializeArial25();
+
+            Time.CurrentTime = 0.0f;
+            textFadingEffect.FadeInLength = 2.0f;
+            Expect.Once.On(sprite).Method("Begin").With(SpriteFlags.AlphaBlend);
+            Expect.Once.On(font).Method("DrawText").
+                With(Is.EqualTo(sprite), Is.Anything, Is.Anything, Is.Anything,
+                Is.EqualTo(Color.FromArgb(0, Color.White))).Will(Return.Value(0));
+            Expect.Once.On(sprite).Method("End");
+            textFadingEffect.Render();
+        }
+
+        [Test]
+        public void TestFadeInHalf()
+        {
+            TestInitializeArial25();
+
+            Time.CurrentTime = 1.0f;
+            textFadingEffect.FadeInLength = 2.0f;
+            Expect.Once.On(sprite).Method("Begin").With(SpriteFlags.AlphaBlend);
+            Expect.Once.On(font).Method("DrawText").
+                With(Is.EqualTo(sprite), Is.Anything, Is.Anything, Is.Anything,
+                Is.EqualTo(Color.FromArgb(127, Color.White))).Will(Return.Value(0));
+            Expect.Once.On(sprite).Method("End");
+            textFadingEffect.Render();
+        }
+
+        [Test]
+        public void TestFullyFaded()
+        {
+            TestInitializeArial25();
+
+            Time.CurrentTime = 4.0f;
+            textFadingEffect.FadeInLength = 2.0f;
+            Expect.Once.On(sprite).Method("Begin").With(SpriteFlags.AlphaBlend);
+            Expect.Once.On(font).Method("DrawText").
+                With(Is.EqualTo(sprite), Is.Anything, Is.Anything, Is.Anything,
+                Is.EqualTo(Color.FromArgb(255, Color.White))).Will(Return.Value(0));
+            Expect.Once.On(sprite).Method("End");
+            textFadingEffect.Render();
+        }
+
+        [Test]
+        public void TestFadeOutHalf()
+        {
+            TestInitializeArial25();
+
+            Time.CurrentTime = 9.0f;
+            textFadingEffect.FadeOutLength = 2.0f;
+            Expect.Once.On(sprite).Method("Begin").With(SpriteFlags.AlphaBlend);
+            Expect.Once.On(font).Method("DrawText").
+                With(Is.EqualTo(sprite), Is.Anything, Is.Anything, Is.Anything,
+                Is.EqualTo(Color.FromArgb(127, Color.White))).Will(Return.Value(0));
+            Expect.Once.On(sprite).Method("End");
+            textFadingEffect.Render();
+        }
+
+        [Test]
+        public void TestFadeOutEnd()
+        {
+            TestInitializeArial25();
+
+            Time.CurrentTime = 10.0f;
+            textFadingEffect.FadeOutLength = 2.0f;
+            Expect.Once.On(sprite).Method("Begin").With(SpriteFlags.AlphaBlend);
+            Expect.Once.On(font).Method("DrawText").
+                With(Is.EqualTo(sprite), Is.Anything, Is.Anything, Is.Anything,
+                Is.EqualTo(Color.FromArgb(0, Color.White))).Will(Return.Value(0));
+            Expect.Once.On(sprite).Method("End");
+            textFadingEffect.Render();
+        }
+
+        [Test]
+        public void TestColorRed()
+        {
+            TestInitializeArial25();
+
+            textFadingEffect.TextColor = Color.Red;
+            Expect.Once.On(sprite).Method("Begin").With(SpriteFlags.AlphaBlend);
+            Expect.Once.On(font).Method("DrawText").
+                With(Is.EqualTo(sprite), Is.Anything, Is.Anything, Is.Anything,
+                Is.EqualTo(Color.FromArgb(255, Color.Red))).Will(Return.Value(0));
+            Expect.Once.On(sprite).Method("End");
+            textFadingEffect.Render();
+        }
+
+        private void InitializeEffect(string fontName, int fontHeight)
+        {
+            textFadingEffect.FontName = fontName;
+            textFadingEffect.FontHeight = fontHeight;
+            ExpectSprite();
+            ExpectFont(fontName, fontHeight);
+            textFadingEffect.Initialize(graphicsFactory, device);
+        }
+
+        private void ExpectFont(string fontName, int fontHeight)
+        {
+            FontDescription description = new FontDescription();
+            description.FaceName = fontName;
+            description.Height = fontHeight;
+            description.Quality = FontQuality.AntiAliased;
+            //description.Weight = FontWeight.Bold;
+            Expect.Once.On(graphicsFactory).
+                Method("CreateFont").
+                With(Is.EqualTo(device), new DescMatcher(description)).Will(Return.Value(font));
+        }
+
+        private void ExpectSprite()
+        {
+            Expect.Once.On(graphicsFactory).
+                Method("CreateSprite").Will(Return.Value(sprite));
+        }
+
+        private class DescMatcher : Matcher
+        {
+            private FontDescription desc;
+
+            public DescMatcher(FontDescription desc)
+            {
+                this.desc = desc;
+            }
+
+            public override void DescribeTo(TextWriter writer)
+            {
+                writer.WriteLine(desc.ToString());
+            }
+
+            public override bool Matches(object o)
+            {
+                if (!(o is FontDescription))
+                    return false;
+                FontDescription desc2 = (FontDescription)o;
+                return desc.CharSet == desc.CharSet && desc.FaceName == desc2.FaceName &&
+                    desc.Height == desc2.Height && desc.IsItalic == desc2.IsItalic &&
+                    desc.MipLevels == desc2.MipLevels && desc.OutputPrecision == desc2.OutputPrecision &&
+                    desc.Quality == desc2.Quality && desc.Weight == desc2.Weight && desc.Width == desc2.Width;
+            }
+        }
+}
 }
