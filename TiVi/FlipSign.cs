419fa4251e14c0e25451d6510ca8507c875d2feb
Henrik Kurelid <henrik@kurelid.se>
2007-07-01 13:52:23 +0000

    Added data for the Kasparov-Deep Blue match in '97.

--- TiVi/FlipSign.cs
+++ TiVi/FlipSign.cs
@@ -46,6 +46,66 @@ namespace TiVi
             scene.AddNode(lights[1]);
         }
 
+        private class PieceInfo
+        {
+            public ChessPiece.PieceColor Color;
+            public ChessPiece.PieceType Type;
+            public string Position;
+            public ChessPiece Piece;
+            public PieceInfo(ChessPiece.PieceColor color, ChessPiece.PieceType type, string position)
+            {
+                Color = color;
+                Type = type;
+                Position = position;
+            }
+        }
+
+        private class PieceMovement
+        {
+            public string StartPos;
+            public string EndPos;
+            public PieceMovement(string startPos, string endPos)
+            {
+                StartPos = startPos;
+                EndPos = endPos;
+            }
+        }
+
+        PieceInfo[] pieceInfo = new PieceInfo[] {
+            new PieceInfo(ChessPiece.PieceColor.White, ChessPiece.PieceType.Pawn, "g6"),
+            new PieceInfo(ChessPiece.PieceColor.White, ChessPiece.PieceType.Pawn, "f5"),
+            new PieceInfo(ChessPiece.PieceColor.White, ChessPiece.PieceType.Pawn, "b4"),
+            new PieceInfo(ChessPiece.PieceColor.White, ChessPiece.PieceType.Pawn, "a3"),
+            new PieceInfo(ChessPiece.PieceColor.White, ChessPiece.PieceType.Pawn, "c2"),
+            new PieceInfo(ChessPiece.PieceColor.White, ChessPiece.PieceType.Bishop, "c3"),
+            new PieceInfo(ChessPiece.PieceColor.White, ChessPiece.PieceType.Bishop, "h3"),
+            new PieceInfo(ChessPiece.PieceColor.White, ChessPiece.PieceType.King, "f2"),
+            new PieceInfo(ChessPiece.PieceColor.White, ChessPiece.PieceType.Rook, "g1"),
+            new PieceInfo(ChessPiece.PieceColor.White, ChessPiece.PieceType.Knight, "e3"),
+            new PieceInfo(ChessPiece.PieceColor.Black, ChessPiece.PieceType.Rook, "d8"),
+            new PieceInfo(ChessPiece.PieceColor.Black, ChessPiece.PieceType.Rook, "e8"),
+            new PieceInfo(ChessPiece.PieceColor.Black, ChessPiece.PieceType.Pawn, "a4"),
+            new PieceInfo(ChessPiece.PieceColor.Black, ChessPiece.PieceType.Pawn, "b5"),
+            new PieceInfo(ChessPiece.PieceColor.Black, ChessPiece.PieceType.Pawn, "c6"),
+            new PieceInfo(ChessPiece.PieceColor.Black, ChessPiece.PieceType.Pawn, "e4"),
+            new PieceInfo(ChessPiece.PieceColor.Black, ChessPiece.PieceType.Pawn, "g4"),
+            new PieceInfo(ChessPiece.PieceColor.Black, ChessPiece.PieceType.Knight, "e5"),
+            new PieceInfo(ChessPiece.PieceColor.Black, ChessPiece.PieceType.Bishop, "f3"),
+            new PieceInfo(ChessPiece.PieceColor.Black, ChessPiece.PieceType.King, "h6"),
+        };
+
+        PieceMovement[] pieceMovement = new PieceMovement[] {
+            new PieceMovement("h3", "g4"),
+            new PieceMovement("f3", "g4"),
+            new PieceMovement("e3", "g4"),
+            new PieceMovement("e5", "g4"),
+            new PieceMovement("g1", "g4"),
+            new PieceMovement("d8", "d5"),
+            new PieceMovement("f5", "f6"),
+            new PieceMovement("d5", "d1"),
+            new PieceMovement("g6", "g7"),
+        };
+
         private void CreatePieces()
         {
             IScene tempScene = new Scene();
@@ -53,72 +113,102 @@ namespace TiVi
             XLoader.AddToScene(tempScene);
             ChessPiece piece;
 
-            piece = new ChessPiece(tempScene, GraphicsFactory, TextureFactory, Device, 
-                ChessPiece.PieceType.Pawn, ChessPiece.PieceColor.White, "g6");
-            chessPieces.Add(piece);
-            piece = new ChessPiece(tempScene, GraphicsFactory, TextureFactory, Device, 
-                ChessPiece.PieceType.Pawn, ChessPiece.PieceColor.White, "f5");
-            chessPieces.Add(piece);
-            piece = new ChessPiece(tempScene, GraphicsFactory, TextureFactory, Device, 
-                ChessPiece.PieceType.Pawn, ChessPiece.PieceColor.White, "b4");
-            chessPieces.Add(piece);
-            piece = new ChessPiece(tempScene, GraphicsFactory, TextureFactory, Device, 
-                ChessPiece.PieceType.Pawn, ChessPiece.PieceColor.White, "a3");
-            chessPieces.Add(piece);
-            piece = new ChessPiece(tempScene, GraphicsFactory, TextureFactory, Device, 
-                ChessPiece.PieceType.Pawn, ChessPiece.PieceColor.White, "c2");
-            chessPieces.Add(piece);
-            piece = new ChessPiece(tempScene, GraphicsFactory, TextureFactory, Device,
-                ChessPiece.PieceType.Bishop, ChessPiece.PieceColor.White, "c3");
-            chessPieces.Add(piece);
-            piece = new ChessPiece(tempScene, GraphicsFactory, TextureFactory, Device,
-                ChessPiece.PieceType.Bishop, ChessPiece.PieceColor.White, "h3");
-            piece.AddPosition(1, 3, "g4", 1);
-            piece.AddPosition(5.2f, 6, "g4-", 1);
-            chessPieces.Add(piece);
-            piece = new ChessPiece(tempScene, GraphicsFactory, TextureFactory, Device,
-                ChessPiece.PieceType.King, ChessPiece.PieceColor.White, "f2");
-            chessPieces.Add(piece);
-            piece = new ChessPiece(tempScene, GraphicsFactory, TextureFactory, Device,
-                ChessPiece.PieceType.Rook, ChessPiece.PieceColor.White, "g1");
-            chessPieces.Add(piece);
-            piece = new ChessPiece(tempScene, GraphicsFactory, TextureFactory, Device,
-                ChessPiece.PieceType.Knight, ChessPiece.PieceColor.White, "e3");
-            chessPieces.Add(piece);
+            foreach (PieceInfo info in pieceInfo)
+            {
+                piece = new ChessPiece(tempScene, GraphicsFactory, TextureFactory, Device,
+                    info.Type, info.Color, info.Position);
+                info.Piece = piece;
+                chessPieces.Add(piece);
+            }
+
+            float time = 1;
+            foreach (PieceMovement movement in pieceMovement)
+            {
+                PieceInfo movePiece = null;
+                PieceInfo removePiece = null;
+                foreach (PieceInfo info in pieceInfo)
+                {
+                    if (info.Position == movement.StartPos)
+                        movePiece = info;
+                    if (info.Position == movement.EndPos)
+                        removePiece = info;
+                }
+                movePiece.Piece.AddPosition(time, 3, movement.EndPos, 1);
+                movePiece.Position = movement.EndPos;
+                if (removePiece != null)
+                {
+                    removePiece.Piece.AddPosition(time, 4, movement.EndPos + "-", 1);
+                    removePiece.Position = "--";
+                }
+                time += 4;
+            }
+
+            //piece = new ChessPiece(tempScene, GraphicsFactory, TextureFactory, Device, 
+            //    ChessPiece.PieceType.Pawn, ChessPiece.PieceColor.White, "g6");
+            //chessPieces.Add(piece);
+            //piece = new ChessPiece(tempScene, GraphicsFactory, TextureFactory, Device, 
+            //    ChessPiece.PieceType.Pawn, ChessPiece.PieceColor.White, "f5");
+            //chessPieces.Add(piece);
+            //piece = new ChessPiece(tempScene, GraphicsFactory, TextureFactory, Device, 
+            //    ChessPiece.PieceType.Pawn, ChessPiece.PieceColor.White, "b4");
+            //chessPieces.Add(piece);
+            //piece = new ChessPiece(tempScene, GraphicsFactory, TextureFactory, Device, 
+            //    ChessPiece.PieceType.Pawn, ChessPiece.PieceColor.White, "a3");
+            //chessPieces.Add(piece);
+            //piece = new ChessPiece(tempScene, GraphicsFactory, TextureFactory, Device, 
+            //    ChessPiece.PieceType.Pawn, ChessPiece.PieceColor.White, "c2");
+            //chessPieces.Add(piece);
+            //piece = new ChessPiece(tempScene, GraphicsFactory, TextureFactory, Device,
+            //    ChessPiece.PieceType.Bishop, ChessPiece.PieceColor.White, "c3");
+            //chessPieces.Add(piece);
+            //piece = new ChessPiece(tempScene, GraphicsFactory, TextureFactory, Device,
+            //    ChessPiece.PieceType.Bishop, ChessPiece.PieceColor.White, "h3");
+            //piece.AddPosition(1, 3, "g4", 1);
+            //piece.AddPosition(5.2f, 6, "g4-", 1);
+            //chessPieces.Add(piece);
+            //piece = new ChessPiece(tempScene, GraphicsFactory, TextureFactory, Device,
+            //    ChessPiece.PieceType.King, ChessPiece.PieceColor.White, "f2");
+            //chessPieces.Add(piece);
+            //piece = new ChessPiece(tempScene, GraphicsFactory, TextureFactory, Device,
+            //    ChessPiece.PieceType.Rook, ChessPiece.PieceColor.White, "g1");
+            //chessPieces.Add(piece);
+            //piece = new ChessPiece(tempScene, GraphicsFactory, TextureFactory, Device,
+            //    ChessPiece.PieceType.Knight, ChessPiece.PieceColor.White, "e3");
+            //chessPieces.Add(piece);
 
             // Black
-            piece = new ChessPiece(tempScene, GraphicsFactory, TextureFactory, Device,
-                ChessPiece.PieceType.Rook, ChessPiece.PieceColor.Black, "d8");
-            chessPieces.Add(piece);
-            piece = new ChessPiece(tempScene, GraphicsFactory, TextureFactory, Device,
-                ChessPiece.PieceType.Rook, ChessPiece.PieceColor.Black, "e8");
-            chessPieces.Add(piece);
-            piece = new ChessPiece(tempScene, GraphicsFactory, TextureFactory, Device,
-                ChessPiece.PieceType.Pawn, ChessPiece.PieceColor.Black, "a4");
-            chessPieces.Add(piece);
-            piece = new ChessPiece(tempScene, GraphicsFactory, TextureFactory, Device,
-                ChessPiece.PieceType.Pawn, ChessPiece.PieceColor.Black, "b5");
-            chessPieces.Add(piece);
-            piece = new ChessPiece(tempScene, GraphicsFactory, TextureFactory, Device,
-                ChessPiece.PieceType.Pawn, ChessPiece.PieceColor.Black, "c6");
-            chessPieces.Add(piece);
-            piece = new ChessPiece(tempScene, GraphicsFactory, TextureFactory, Device,
-                ChessPiece.PieceType.Pawn, ChessPiece.PieceColor.Black, "e4");
-            chessPieces.Add(piece);
-            piece = new ChessPiece(tempScene, GraphicsFactory, TextureFactory, Device,
-                ChessPiece.PieceType.Pawn, ChessPiece.PieceColor.Black, "g4");
-            piece.AddPosition(1.5F, 4, "g4-", 1);
-            chessPieces.Add(piece);
-            piece = new ChessPiece(tempScene, GraphicsFactory, TextureFactory, Device,
-                ChessPiece.PieceType.Knight, ChessPiece.PieceColor.Black, "e5");
-            chessPieces.Add(piece);
-            piece = new ChessPiece(tempScene, GraphicsFactory, TextureFactory, Device,
-                ChessPiece.PieceType.Bishop, ChessPiece.PieceColor.Black, "f3");
-            piece.AddPosition(5.0f, 3, "g4", 0.2f);
-            chessPieces.Add(piece);
-            piece = new ChessPiece(tempScene, GraphicsFactory, TextureFactory, Device,
-                ChessPiece.PieceType.King, ChessPiece.PieceColor.Black, "h6");
-            chessPieces.Add(piece);
+            //piece = new ChessPiece(tempScene, GraphicsFactory, TextureFactory, Device,
+            //    ChessPiece.PieceType.Rook, ChessPiece.PieceColor.Black, "d8");
+            //chessPieces.Add(piece);
+            //piece = new ChessPiece(tempScene, GraphicsFactory, TextureFactory, Device,
+            //    ChessPiece.PieceType.Rook, ChessPiece.PieceColor.Black, "e8");
+            //chessPieces.Add(piece);
+            //piece = new ChessPiece(tempScene, GraphicsFactory, TextureFactory, Device,
+            //    ChessPiece.PieceType.Pawn, ChessPiece.PieceColor.Black, "a4");
+            //chessPieces.Add(piece);
+            //piece = new ChessPiece(tempScene, GraphicsFactory, TextureFactory, Device,
+            //    ChessPiece.PieceType.Pawn, ChessPiece.PieceColor.Black, "b5");
+            //chessPieces.Add(piece);
+            //piece = new ChessPiece(tempScene, GraphicsFactory, TextureFactory, Device,
+            //    ChessPiece.PieceType.Pawn, ChessPiece.PieceColor.Black, "c6");
+            //chessPieces.Add(piece);
+            //piece = new ChessPiece(tempScene, GraphicsFactory, TextureFactory, Device,
+            //    ChessPiece.PieceType.Pawn, ChessPiece.PieceColor.Black, "e4");
+            //chessPieces.Add(piece);
+            //piece = new ChessPiece(tempScene, GraphicsFactory, TextureFactory, Device,
+            //    ChessPiece.PieceType.Pawn, ChessPiece.PieceColor.Black, "g4");
+            //piece.AddPosition(1.5F, 4, "g4-", 1);
+            //chessPieces.Add(piece);
+            //piece = new ChessPiece(tempScene, GraphicsFactory, TextureFactory, Device,
+            //    ChessPiece.PieceType.Knight, ChessPiece.PieceColor.Black, "e5");
+            //chessPieces.Add(piece);
+            //piece = new ChessPiece(tempScene, GraphicsFactory, TextureFactory, Device,
+            //    ChessPiece.PieceType.Bishop, ChessPiece.PieceColor.Black, "f3");
+            //piece.AddPosition(5.0f, 3, "g4", 0.2f);
+            //chessPieces.Add(piece);
+            //piece = new ChessPiece(tempScene, GraphicsFactory, TextureFactory, Device,
+            //    ChessPiece.PieceType.King, ChessPiece.PieceColor.Black, "h6");
+            //chessPieces.Add(piece);
         }
 
         private void CreateCameraInterpolator()
