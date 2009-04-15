9e0956f17ac92b069fb5d42a31fe0b2fa3e15271
Henrik Kurelid <henrik@kurelid.se>
2009-04-15 19:08:37 +0000

    Added negate generator.

---
+++ TextureBuilder/Negate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Dope.DDXX.TextureBuilder
+{
+    public class Negate : Generator
+    {
+        public Negate()
+            : base(1)
+        {
+        }
+
+        public override Vector4 GetPixel(Vector2 textureCoordinate, Vector2 texelSize)
+        {
+            return Vector4.One - GetInputPixel(0, textureCoordinate, texelSize);
+        }
+    }
+}
